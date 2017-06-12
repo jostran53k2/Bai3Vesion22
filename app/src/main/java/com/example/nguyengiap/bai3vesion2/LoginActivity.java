04560cf46cd25efed17cccce99765187dd786958
jostran53k2 <trannguyengiap53k2@gmail.com>
2017-06-12 20:59:01 +0700

    add LoginActivity class

---
+++ app/src/main/java/com/example/nguyengiap/bai3vesion2/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.nguyengiap.bai3vesion2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nguyen Giap on 12/06/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
